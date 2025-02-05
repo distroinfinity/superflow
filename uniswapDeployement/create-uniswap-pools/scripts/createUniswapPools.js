b4fe6b11f2e161888432f29a2fac95b38804fa7f
zod-3 <yashhrr121@gmail.com>
2025-02-06 02:52:19 +0530

    add prompts

--- uniswapDeployement/create-uniswap-pools/scripts/createUniswapPools.js
+++ uniswapDeployement/create-uniswap-pools/scripts/createUniswapPools.js
@@ -9,67 +9,74 @@ const { ethers: hreEthers } = require("hardhat");
 const { UNISWAP_FACTOR_ABI, UNISWAP_V3_POOL_ABI } = require("./abi.js");
 const { Percent, Token } = require("@uniswap/sdk-core");
 const ERC20_ABI = require("../artifacts/contracts/Token.sol/Token.json").abi;
-// require("dotenv").config();
+require("dotenv").config();
+
+const readline = require('readline');
 
 const token1Info = {
-    arbitrumsepolia : {
+    celo: {
         NonfungiblePositionManager: "0x6b2937Bde17889EDCf8fbD8dE31C3C2a70Bc4d65",
-        UniswapV3Factory: "0x248AB79Bbb9bC29bB72f7Cd42F17e054Fc40188e",
-        token1Address: "0xFE8348081Fe38B20ECd971c708a8471F4Dc88D09"
+        UniswapV3Factory: "0x248AB79Bbb9bC29bB72f7Cd42F17e054Fc40188e"
     },
-    basesepolia: {
-        NonfungiblePositionManager: "0x27F971cb582BF9E50F397e4d29a5C7A34f11faA2",
-        UniswapV3Factory: "0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24",
-        token1Address:"0x65AC665a2E700A7D53ec16c864D92cebb8926635"
-    },
-    optimismsepolia: {
-        NonfungiblePositionManager: "0x27F971cb582BF9E50F397e4d29a5C7A34f11faA2",
-        UniswapV3Factory: "0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24",
-        token1Address: "0x65AC665a2E700A7D53ec16c864D92cebb8926635"
-    },
-    alfajores: {
-        NonfungiblePositionManager: "",
-        UniswapV3Factory: "",
-        token1Address: ""
-    },
-}
+};
 
 async function main() {
-    var token0Address = process.env.BASETOKEN; // tge token
-    var token1Address = token1Info[process.env.CHAIN_NAME].token1Address; // collateral token TODO: MANU
-    // (0.05, 0.3, 1, 0.01)
-    var fee = (process.env.POOL_FEE) * 1000;
-    var token0Decimals = 18;
-    var token1Decimals = 18;
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-    var price = encodePriceSqrt(1, 1);
-    // TODO : MANU
-    var npmca = token1Info[process.env.CHAIN_NAME].NonfungiblePositionManager;             // NonfungiblePositionManager
-    var uniswapFactoryAddress = token1Info[process.env.CHAIN_NAME].UniswapV3Factory;       // UniswapV3Factory
-    var amount0 = ethers.utils.parseUnits(process.env.BASETOKEN_AMOUNT.toString(), 18);
-    var amount1 = ethers.utils.parseUnits(process.env.QUOTE_TOKEN_AMOUNT.toString(), 18);
-    var chainID = Number(process.env.CHAINID)
+    const askQuestion = (query) => new Promise((resolve) => rl.question(query, resolve));
 
-    // console.log("Started");
-    const uniswapFactoryContract = await getContract(uniswapFactoryAddress, UNISWAP_FACTOR_ABI);
-    const token0 = await getContract(token0Address, ERC20_ABI);
-    const token1 = await getContract(token1Address, ERC20_ABI);
+    try {
+        console.log("Prompting user for input...");
+        const token0Address = await askQuestion("Enter the base token address: ");
+        const token1Address = await askQuestion("Enter the collateral token address: ");
+        const fee = parseFloat(await askQuestion("Enter the pool fee among these 0.05, 0.3, 1, 0.01 (e.g., 0.05 for 5%): ")) * 10000;
+        const basetokenAmount = await askQuestion("Enter the base token amount: ");
+        const quoteTokenAmount = await askQuestion("Enter the quote token amount: ");
+        const token1Price = parseFloat(await askQuestion("Enter the price of token1 relative to token0 (e.g., 1.5): "));
+        const token0Price = parseFloat(await askQuestion("Enter the price of token0 relative to token1 (e.g., 1): "));
 
-    await mintAndApprove(amount0, amount1, token0Address, token1Address, npmca);
+        rl.close();
 
-    var poolAddress = await uniswapFactoryContract.getPool(token0Address, token1Address, fee);
-    var deployer = await hreEthers.getSigner();
-    if (poolAddress === '0x0000000000000000000000000000000000000000') {
-        // console.log("Creating pool");
-        poolAddress = await createPool(uniswapFactoryContract, token0Address, token1Address, fee);
+        const price = encodePriceSqrt(token1Price, token0Price);
+
+        const token0Decimals = 18;
+        const token1Decimals = 18;
+
+        const npmca = token1Info.celo.NonfungiblePositionManager; 
+        const uniswapFactoryAddress = token1Info.celo.UniswapV3Factory;
+        const amount0 = ethers.utils.parseUnits(basetokenAmount.toString(), 18);
+        const amount1 = ethers.utils.parseUnits(quoteTokenAmount.toString(), 18);
         
-        await initializePool(poolAddress, price, deployer);
-    }
-    console.log(poolAddress);
-    await addLiquidityToPool(poolAddress, deployer, chainID, token0Decimals, token1Decimals, token0, token1, amount0, amount1, fee, npmca);
-    // console.log("Added liquidity");
+        //chain id can be configured 
+        const chainID = 44787;
 
-    // console.log("Creating pool done");
+        const uniswapFactoryContract = await getContract(uniswapFactoryAddress, UNISWAP_FACTOR_ABI);
+        const token0 = await getContract(token0Address, ERC20_ABI);
+        const token1 = await getContract(token1Address, ERC20_ABI);
+
+        await mintAndApprove(amount0, amount1, token0Address, token1Address, npmca);
+
+        console.log("Checking for existing pool...");
+        let poolAddress = await uniswapFactoryContract.getPool(token0Address, token1Address, fee);
+        console.log(`Pool Address Before Creation: ${poolAddress}`);
+
+        const deployer = await hreEthers.getSigner();
+        if (poolAddress === ethers.constants.AddressZero) {
+            console.log("Pool does not exist. Creating pool...");
+            poolAddress = await createPool(uniswapFactoryContract, token0Address, token1Address, fee);
+            console.log(`Pool Address After Creation: ${poolAddress}`);
+            await initializePool(poolAddress, price, deployer);
+        }
+
+        console.log("Adding liquidity to the pool...");
+        await addLiquidityToPool(poolAddress, deployer, chainID, token0Decimals, token1Decimals, token0, token1, amount0, amount1, fee, npmca);
+        console.log("Liquidity added successfully.");
+    } catch (error) {
+        console.error("Error occurred during execution:", error.message || error);
+    }
 }
 
 function encodePriceSqrt(token1Price, token0Price) {
@@ -90,7 +97,6 @@ async function getPoolState(poolContract) {
         feeProtocol: slot[5],
         unlocked: slot[6],
     };
-
     return PoolState;
 }
 
@@ -105,41 +111,31 @@ async function mintAndApprove(amount0, amount1, token0Address, token1Address, np
     var token0 = new ethers.Contract(token0Address, ERC20_ABI, deployer);
     var token1 = new ethers.Contract(token1Address, ERC20_ABI, deployer);
 
-    // await token0.mint(amount0);
-    // await token1.mint(amount1);
-
     await token0.approve(npmca, amount0);
-
     await token1.approve(npmca, amount1);
 }
 
-async function createPool(uniswapFactory_contract, token1Address, token2Address, fee) {
-    var txs;
-    txs = await uniswapFactory_contract.createPool(
+async function createPool(uniswapFactoryContract, token1Address, token2Address, fee) {
+    var txs = await uniswapFactoryContract.createPool(
         token1Address.toLowerCase(),
         token2Address.toLowerCase(),
         fee,
-        {
-            gasLimit: 10000000,
-        }
+        { gasLimit: 10000000 }
     );
     await txs.wait();
 
-    const poolAdd = await uniswapFactory_contract.getPool(token1Address, token2Address, fee, {
+    const poolAdd = await uniswapFactoryContract.getPool(token1Address, token2Address, fee, {
         gasLimit: 3000000,
     });
-    // console.log('Pool address', poolAdd);
     return poolAdd;
 }
 
 async function initializePool(poolAdd, price, signer) {
     const poolContract = new ethers.Contract(poolAdd, UNISWAP_V3_POOL_ABI, signer);
-
     var txs = await poolContract.initialize(price.toString(), {
         gasLimit: 3000000,
     });
     await txs.wait();
-    // console.log('Pool Initialized');
 }
 
 async function addLiquidityToPool(
@@ -150,58 +146,75 @@ async function addLiquidityToPool(
     Token2_decimals,
     token_contract1,
     token_contract2,
-    amount0, amount1,
+    amount0,
+    amount1,
     fee,
     npmca
 ) {
-    const poolContract = new ethers.Contract(poolAdd, UNISWAP_V3_POOL_ABI, deployer);
-    var state = await getPoolState(poolContract);
+    try {
+       
+        // Get pool state
+        const poolContract = new ethers.Contract(poolAdd, UNISWAP_V3_POOL_ABI, deployer);
+        const state = await getPoolState(poolContract);
+        
+        // Initialize token objects
+        const Token1 = new Token(chainId, token_contract1.address, Token1_decimals);
+        const Token2 = new Token(chainId, token_contract2.address, Token2_decimals);
+       
+        // Configure pool
+        const configuredPool = new Pool(
+            Token1,
+            Token2,
+            fee,
+            state.sqrtPriceX96.toString(),
+            state.liquidity.toString(),
+            state.tick
+        );
+        // Create position
+        const position = Position.fromAmounts({
+            pool: configuredPool,
+            tickLower:
+                nearestUsableTick(configuredPool.tickCurrent, configuredPool.tickSpacing) -
+                configuredPool.tickSpacing * 2,
+            tickUpper:
+                nearestUsableTick(configuredPool.tickCurrent, configuredPool.tickSpacing) +
+                configuredPool.tickSpacing * 2,
+            amount0: amount0.toString(),
+            amount1: amount1.toString(),
+            useFullPrecision: false,
+        });
+        // Define mint options
+        const mintOptions = {
+            recipient: deployer.address,
+            deadline: Math.floor(Date.now() / 1000) + 60 * 20, // 20 minutes from now
+            slippageTolerance: new Percent(50, 10_000), // 0.5%
+        };
 
+        // Generate calldata and value for the transaction
+        const { calldata, value } = NonfungiblePositionManager.addCallParameters(position, mintOptions);
 
-    const Token1 = new Token(chainId, token_contract1.address, Token1_decimals);
-    const Token2 = new Token(chainId, token_contract2.address, Token2_decimals);
+        // Check token balances
+        const balance0 = await token_contract1.balanceOf(deployer.address);
+        const balance1 = await token_contract2.balanceOf(deployer.address);
 
-    const configuredPool = new Pool(
-        Token1,
-        Token2,
-        fee,
-        state.sqrtPriceX96.toString(),
-        state.liquidity.toString(),
-        state.tick
-    );
+        // Check token allowances
+        const allowance0 = await token_contract1.allowance(deployer.address, npmca);
+        const allowance1 = await token_contract2.allowance(deployer.address, npmca);
 
-    const position = Position.fromAmounts({
-        pool: configuredPool,
-        tickLower:
-            nearestUsableTick(configuredPool.tickCurrent, configuredPool.tickSpacing) -
-            configuredPool.tickSpacing * 2,
-        tickUpper:
-            nearestUsableTick(configuredPool.tickCurrent, configuredPool.tickSpacing) +
-            configuredPool.tickSpacing * 2,
-        amount0: amount0.toString(),
-        amount1: amount1.toString(),
-        useFullPrecision: false,
-    });
+        // Create and send the transaction
+        const transaction = {
+            data: calldata,
+            to: npmca,
+            value: value,
+            from: deployer.address,
+            gasLimit: 10000000,
+        };
 
-    const mintOptions = {
-        recipient: deployer.address,
-        deadline: Math.floor(Date.now() / 1000) + 60 * 20,
-        slippageTolerance: new Percent(50, 10_000),
-    };
-
-    const { calldata, value } = NonfungiblePositionManager.addCallParameters(position, mintOptions);
-
-    const transaction = {
-        data: calldata,
-        to: npmca,
-        value: value,
-        from: deployer.address,
-        gasLimit: 10000000
-    };
-    // console.log('Transacting');
-    const txRes = await deployer.sendTransaction(transaction);
-    await txRes.wait();
-    // console.log('Added liquidity');
+        const txRes = await deployer.sendTransaction(transaction);
+        const receipt = await txRes.wait();
+    } catch (error) {
+        console.error("Error occurred while adding liquidity:", error);
+    }
 }
 
 main().catch(console.log);
