b2db7b19aef28e6caa470c638d83b10e53045815
Yash Rajput <yashrajput2911@gmail.com>
2025-03-18 01:01:02 +0530

    frontend folder added

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TokenDeployer from './components/deploytoken';
+
+function App() {
+  return (
+    <div className="App">
+    <TokenDeployer/>
+    </div>
+  );
+}
+
+export default App;
